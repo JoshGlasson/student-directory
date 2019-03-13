0b824efbe10fa646fb4c54a11ba6d571269f27ff
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-13 15:35:06 +0000

    finished directory

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 # user input for students
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names and cohorts of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
@@ -8,13 +8,17 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    cohort = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    students << {name: name, cohort: cohort}
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} student"
+    end
+    # get another name and cohort from the user
     name = gets.chomp
   end
-  # return the array of students
   students
 end
 
@@ -24,13 +28,22 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
+#    if student[:name][0,1] == "J"  # ---> code for printing out names beginning with certain letter
+#    if student[:name].length < 5   # ---> code for printing out names under certain length
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+#    end # ---> end for first letter of names
+#    end # ---> end for certain length of names
+end
+
+def print_footer(students)
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student"
+  else
+    puts "Overall, we have #{students.count} great students"
   end
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
-end
-#nothing happens until we call the methods
+
 students = input_students
 print_header
 print(students)
