0ad4d7562ce61dbd1b0625da2d70a44ed72a98f6
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-13 16:05:27 +0000

    added start of interactive menu

--- directory.rb
+++ directory.rb
@@ -1,9 +1,32 @@
+def interactive_menu
+  students = []
+  loop do
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+  # 2. read the input and save it into a variable
+  selection = gets.chomp
+  # 3. do what the user has asked
+  case selection
+    when "1"
+      students = input_students(students)
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 # user input for students
-def input_students
+def input_students(students)
   puts "Please enter the names and cohorts of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -44,7 +67,4 @@ def print_footer(students)
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
