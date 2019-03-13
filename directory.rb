7043318a5a786d6399d30d9befa4ca74d60f839d
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-13 16:16:14 +0000

    cleaned up interactive menu

--- directory.rb
+++ directory.rb
@@ -1,30 +1,39 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-  # 1. print the menu and ask the user what to do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items
-  # 2. read the input and save it into a variable
-  selection = gets.chomp
-  # 3. do what the user has asked
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
   case selection
     when "1"
-      students = input_students(students)
+      @students = input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
-    end
   end
 end
 
 # user input for students
-def input_students(students)
+def input_students
   puts "Please enter the names and cohorts of the students"
   puts "To finish, just hit return twice"
   # get the first name
@@ -33,24 +42,24 @@ def input_students(students)
   while !name.empty? do
     cohort = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: cohort}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{@students.count} student"
     end
     # get another name and cohort from the user
     name = gets.chomp
   end
-  students
+  @students
 end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(students)
-  students.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
 #    if student[:name][0,1] == "J"  # ---> code for printing out names beginning with certain letter
 #    if student[:name].length < 5   # ---> code for printing out names under certain length
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
@@ -59,11 +68,11 @@ def print(students)
 #    end # ---> end for certain length of names
 end
 
-def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student"
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
