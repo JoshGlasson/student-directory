6b81dd3cede4f4b3f15104ca43612c8ee049445f
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-14 21:48:15 +0000

    adjusted menu options 3 and 4 to reflect ability to save to any csv file named

--- directory.rb
+++ directory.rb
@@ -10,8 +10,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to csv file"
+  puts "4. Load the list from csv file"
   puts "9. Exit"
 end
 
